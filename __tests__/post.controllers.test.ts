18a0f926906df029c3ae99c9212a178d717fc169
Kriss-Hanna <kristofferson.hanna@gmail.com>
2023-05-04 17:09:19 +0200

    add test controllers

--- __tests__/post.controllers.test.ts
+++ __tests__/post.controllers.test.ts
@@ -1,9 +1,37 @@
 import request from "supertest";
 import app from "../app";
+import PostModel from "../src/models/post.models";
 
 describe("Post Routes", () => {
-  test("Get all posts status code", async () => {
+  test("Should return all posts and return a 200 status code", async () => {
+    /*  const posts = await PostModel.findAll(); */
+    const date = new Date();
+
+    const posts: PostModel = {
+      id: "id n°1",
+      title: "Post 1",
+      imageurl: "Lorem ipsum",
+      description: "Dolor sit amet",
+      createdAt: date,
+      updatedAt: date,
+    };
+
+    jest.spyOn(PostModel, "findAll").mockResolvedValue(posts);
+
     const res = await request(app).get("/posts");
+
     expect(res.status).toBe(200);
+    expect(res.body).toEqual(posts);
+  });
+
+  it("should handle errors and return a 500 status code", async () => {
+    jest
+      .spyOn(PostModel, "findAll")
+      .mockRejectedValue(new Error("Database error"));
+
+    const res = await request(app).get("/posts");
+
+    expect(res.status).toBe(500);
+    expect(res.body).toEqual({ error: "Unable to get all posts." });
   });
 });
