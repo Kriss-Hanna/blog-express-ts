5091dd3737439c2963e85769f113f3c7bfec7ddc
Kriss-Hanna <kristofferson.hanna@gmail.com>
2023-05-29 14:38:31 +0200

    create user logic

--- app.ts
+++ app.ts
@@ -5,6 +5,7 @@ const cors = require("cors");
 import "dotenv/config";
 import PostController from "./src/controllers/post.controllers";
 import ContactFormController from "./src/controllers/contactform.controllers";
+import UserController from "./src/controllers/user.controllers";
 
 const PORT = 8000;
 
@@ -21,6 +22,9 @@ app.delete("/posts/:id", PostController.deletePost);
 
 // Form routes
 app.post("/contact", ContactFormController.createPost);
+
+// User routes
+app.post("/user", UserController.createUser);
 
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, console.log(`SERVER STARTED ON ${PORT}`));
